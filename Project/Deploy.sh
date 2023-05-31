4231cbe668bc3a8f0ada123868691659ca04db9e
Abanob Ayad <118727612+AbanobAyad22@users.noreply.github.com>
2023-05-31 23:31:04 +0300

    project

---
+++ Project/Deploy.sh
@@ -0,0 +1,7 @@
+kubectl apply -f Namespace.yml
+kubectl apply -f LoadBalancer.yml
+kubectl apply -f PV.yml
+kubectl apply -f PVC.yml
+kubectl apply -f ServiceAccount.yml
+kubectl apply -f jenkins-deployment.yml
+
